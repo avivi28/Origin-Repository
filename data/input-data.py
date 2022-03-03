5219562012919aea5b7a6b00711d2382899e3865
HoneyPaw <honeypaw.production@gmail.com>
2022-03-03 18:04:03 +0800

    upload two API for searching attractions

---
+++ data/input-data.py
@@ -0,0 +1,32 @@
+from audioop import add
+import json
+import mysql.connector
+from mysql.connector import pooling
+
+poolname="mysqlpool"
+poolsize=3
+
+connectionpool=mysql.connector.pooling.MySQLConnectionPool(pool_name=poolname,pool_size=poolsize, pool_reset_session=True, host='localhost', user='root', password="password", database='travel')
+db = connectionpool.get_connection()
+cursor = db.cursor()
+
+f=open('taipei-attractions.json', encoding="utf-8")
+data=json.load(f)
+sorted_result=data['result']['results']
+for all_data in sorted_result:
+    id=all_data["_id"]
+    name=all_data["stitle"]
+    cat=all_data["CAT2"]
+    description=all_data["xbody"]
+    address=all_data["address"][0:3]+all_data["address"][5:]
+    transport=all_data["info"]
+    mrt=all_data["MRT"]
+    latitude=all_data["latitude"]
+    longitude=all_data["longitude"]
+    image=all_data["file"]
+    image=image.split("https")
+    sorted_image=[]
+    for x in image:
+        if x.endswith(".jpg") or x.endswith(".JPG"):
+            sorted_image.append("https"+x)
+f.close() # close the file after used
