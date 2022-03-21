b4ad1e0fec99526a2db423e303be5838e71495f6
avivi28 <avivichan.ccsu67@gmail.com>
2022-03-22 05:54:00 +0800

    create layout.html as skeleton & finish the pop up function

--- static/attraction.js
+++ static/attraction.js
@@ -101,3 +101,29 @@ function priceChange() {
 		targetContext.innerHTML = '新台幣 2000 元';
 	}
 }
+
+//-------modal event of signin button------------
+let modal = document.getElementById('signin-modal');
+function showModal() {
+	modal.style.display = 'block';
+}
+
+//-------switch between signin model & register modal----------------
+let returnButton = document.getElementById('return-register');
+let registerModal = document.getElementById('register-modal');
+function returnRegister() {
+	modal.style.display = 'none';
+	registerModal.style.display = 'block';
+}
+
+function returnSignIn() {
+	modal.style.display = 'block';
+	registerModal.style.display = 'none';
+}
+
+//--------close the modal-------------
+let closeButton = document.getElementById('close');
+function closeModal() {
+	modal.style.display = 'none';
+	registerModal.style.display = 'none';
+}
--- static/index.js
+++ static/index.js
@@ -122,3 +122,29 @@ function onEnterView(entries, observer) {
 		}
 	});
 }
+
+//-------modal event of signin button------------
+let modal = document.getElementById('signin-modal');
+function showModal() {
+	modal.style.display = 'block';
+}
+
+//-------switch between signin model & register modal----------------
+let returnButton = document.getElementById('return-register');
+let registerModal = document.getElementById('register-modal');
+function returnRegister() {
+	modal.style.display = 'none';
+	registerModal.style.display = 'block';
+}
+
+function returnSignIn() {
+	modal.style.display = 'block';
+	registerModal.style.display = 'none';
+}
+
+//--------close the modal-------------
+let closeButton = document.getElementById('close');
+function closeModal() {
+	modal.style.display = 'none';
+	registerModal.style.display = 'none';
+}
