487161a50e41b4aeff6a7c26e6fea9f3cc6406bd
avivi28 <avivichan.ccsu67@gmail.com>
2022-03-27 09:02:48 +0800

    Altered into env. virables

--- model/database.py
+++ model/database.py
@@ -1,13 +1,18 @@
 import mysql.connector
 from mysql.connector import pooling
+import os
+from dotenv import load_dotenv
+
+load_dotenv("./data/.env")
 
 poolname="mysqlpool"
 poolsize=3
+mysql_password = os.getenv("mysql_password")
 
 CONFIG={
    "host":'localhost', 
    "user":'root', 
-   "password":"password", 
+   "password":mysql_password, 
    "database":'travel',
 }
 db=mysql.connector.connect(pool_name=poolname,pool_size=poolsize, pool_reset_session=True, auth_plugin='mysql_native_password',**CONFIG)
