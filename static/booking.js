d64d3d909f128c23c9c11e3250cdd1eb96d240d9
avivi28 <avivichan.ccsu67@gmail.com>
2022-03-28 06:30:35 +0800

    Finished booking.html layout

---
+++ static/booking.js
@@ -0,0 +1,6 @@
+function prettierCreditCardInput() {
+	// this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');
+	// return this.value.replace(
+	// 	value.replace(/\W/gi, '').replace(/(.{4})/g, '$1 ')
+	// );
+}
